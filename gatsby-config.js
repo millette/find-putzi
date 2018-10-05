3fa20fcaaca094a50b87d68695ae3396f7f6f7f6
Robin Millette <robin@millette.info>
2018-10-04 20:53:54 -0400

    Initial commit.

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// self
+const { name, description } = require('./package.json')
+
+const cwd = process.cwd()
+
+module.exports = {
+  pathPrefix: `/${name}`,
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${cwd}/data/`
+      }
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        // icon: 'src/images/logo.png',
+        name: 'Québec Projects (GitHub) - RoLLodeQc',
+        short_name: 'Projets Qc',
+        description,
+        lang: 'en',
+        start_url: './',
+        scope: `/${name}/`,
+        background_color: 'white',
+        theme_color: 'black',
+        display: 'fullscreen'
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline'
+  ]
+}
---
+++ src/pages/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+const style = {
+  width: '45%',
+  float: 'left'
+}
+
+export default ({ data: { allAllCatsV4Json: { totalCount, edges } } }) => {
+  const zz = edges.map(({ node }) => {
+    const imgs = node.imgs
+      ? node.imgs.filter((x) => x !== 'http://g.petango.com/shared/Photo-Not-Available-cat.gif')
+      : []
+    return {
+      ...node,
+      imgs
+    }
+  })
+  .filter(({ imgs }) => imgs && imgs.length)
+
+  // <pre>{JSON.stringify(zz, null, '  ')}</pre>
+  return (
+    <div>
+      <p>oh my! We have {totalCount}</p>
+      {zz.map(({ imgs, url, g3 }) => (
+        <div style={style}>
+          {imgs.map((i) => (
+            <img src={i} alt={g3.name} />
+          ))}
+          <h2>{g3.name} ({g3.Sexe}) <small>#{g3.No_de_R_f_rence}</small></h2>
+          {g3.POSTING_DATE} {g3.Taille} {g3.Couleur} {g3.Race}{g3._ge && ` de ${g3._ge} ans`}<br />
+          Dégriffé? {g3.D_griff_}<br />
+          <a href={url}>Lien original</a>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export const query = graphql`
+  query {
+    allAllCatsV4Json (
+      sort: {
+        fields: [g3___POSTING_DATE]
+        order: DESC
+      }
+    ) {
+      totalCount
+      edges {
+        node {
+          imgs
+          g3 {
+            name
+            No_de_R_f_rence
+            Esp_ce
+            Sexe
+            Race
+            Taille
+            Couleur
+            D_griff_
+            POSTING_DATE
+            _ge
+          }
+          url
+        }
+      }
+    }
+  }
+`
