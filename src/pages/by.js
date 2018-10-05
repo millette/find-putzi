05801abfe1eb70de3200168475255a46362a81ec
Robin Millette <robin@millette.info>
2018-10-05 14:28:23 -0400

    With colors.

--- src/pages/by.js
+++ src/pages/by.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 
+const colors = {
+  Butterscotch: '#c67941',
+  Charcoal: '#1b0227',
+  Cream: '#c9bb53',
+  Rust: '#df6b2b'
+}
+
+const colorStyle = fieldValue => ({
+  backgroundColor: colors[fieldValue] || fieldValue,
+  transformOrigin: '50%',
+  padding: '0.25rem',
+  margin: '0.25rem 0.25rem 0.25rem 9rem',
+  listStyle: 'none'
+})
+
 export default ({
   data: {
     sex: { sex },
@@ -41,8 +56,10 @@ export default ({
       <h2>By color</h2>
       <ul>
         {colors.map(({ fieldValue, totalCount }) => (
-          <li key={fieldValue}>
-            {totalCount} {fieldValue}
+          <li key={fieldValue} style={colorStyle(fieldValue)}>
+            <span style={{ marginLeft: '-9rem' }}>
+              {totalCount} {fieldValue}
+            </span>
           </li>
         ))}
       </ul>
@@ -60,48 +77,31 @@ export default ({
 
 export const query = graphql`
   query {
-    sex: allAllCatsV5Json(
-      sort: { fields: [postingDate], order: DESC }
-      filter: { noDeReference: { ne: null } }
-    ) {
+    sex: allAllCatsV5Json(filter: { noDeReference: { ne: null } }) {
       sex: group(field: sexe) {
         fieldValue
         totalCount
       }
     }
-    size: allAllCatsV5Json(
-      sort: { fields: [postingDate], order: DESC }
-      filter: { noDeReference: { ne: null } }
-    ) {
+    size: allAllCatsV5Json(filter: { noDeReference: { ne: null } }) {
       size: group(field: taille) {
         fieldValue
         totalCount
       }
     }
-
-    declawd: allAllCatsV5Json(
-      sort: { fields: [postingDate], order: DESC }
-      filter: { noDeReference: { ne: null } }
-    ) {
+    declawd: allAllCatsV5Json(filter: { noDeReference: { ne: null } }) {
       declawd: group(field: degriffe) {
         fieldValue
         totalCount
       }
     }
-
-    colors: allAllCatsV5Json(
-      sort: { fields: [postingDate], order: DESC }
-      filter: { noDeReference: { ne: null } }
-    ) {
+    colors: allAllCatsV5Json(filter: { noDeReference: { ne: null } }) {
       colors: group(field: couleur) {
         fieldValue
         totalCount
       }
     }
-    races: allAllCatsV5Json(
-      sort: { fields: [postingDate], order: DESC }
-      filter: { noDeReference: { ne: null } }
-    ) {
+    races: allAllCatsV5Json(filter: { noDeReference: { ne: null } }) {
       races: group(field: race) {
         fieldValue
         totalCount
