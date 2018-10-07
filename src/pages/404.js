4eb552670f236f8a89c531f7378e206917da7eef
Robin Millette <robin@millette.info>
2018-10-06 21:13:38 -0400

    404 page.

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default () => (
+  <div>
+    <h1>Not found</h1>
+    <p>
+      Try <Link to='/by/'>categories</Link>.
+    </p>
+  </div>
+)
